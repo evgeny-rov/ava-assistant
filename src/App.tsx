237ed86f7a5997905234e8e1b440785823980342
Evgeny Rov <bitsinmyhead@gmail.com>
2020-06-27 14:50:15 +0800

    app init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Ava from './components/Ava'
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Ava />
+    </div>
+  );
+}
+
+export default App;
