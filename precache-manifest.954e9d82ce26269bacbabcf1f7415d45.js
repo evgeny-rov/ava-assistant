0d52b7e8cb3c175dc9dcdd4e1ba793f897ef69d4
Evgeny Rov <bitsinmyhead@gmail.com>
2020-09-13 13:46:20 +0800

    ghpages demo build

---
+++ precache-manifest.954e9d82ce26269bacbabcf1f7415d45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e16c210c8e0ce79df7c7a645a68b3f7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2fe3d1f445aef44bce99",
+    "url": "./static/css/main.6e31bafa.chunk.css"
+  },
+  {
+    "revision": "ddf507c45435c7ba4532",
+    "url": "./static/js/2.2848ca0c.chunk.js"
+  },
+  {
+    "revision": "fdc84dfb581e6fb8755dd1b2b01a50e1",
+    "url": "./static/js/2.2848ca0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fe3d1f445aef44bce99",
+    "url": "./static/js/main.8eae63ff.chunk.js"
+  },
+  {
+    "revision": "cb378a1c83feaee6b17f",
+    "url": "./static/js/runtime-main.ba7bc7fd.js"
+  }
+]);
